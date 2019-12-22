8b88a6e556a4a09be4aaa26c684f7da7f3ec179e
Alexander Meißner <AlexanderMeissner@gmx.net>
2019-12-22 21:20:21 +0100

    Implements ram and a rom dummy

--- src/bus.rs
+++ src/bus.rs
@@ -1,12 +1,16 @@
 pub struct BUS {
-
+    rom: [u8; 4],
+    pub ram: Vec<u8>
 }
 
 impl BUS {
     pub fn new() -> Self {
-        Self {
-
-        }
+        let mut bus = Self {
+            rom: [0, 0, 0, 0],
+            ram: Vec::with_capacity(0xA0000)
+        };
+        bus.ram.resize(bus.ram.capacity(), 0);
+        bus
     }
 
     pub fn physical_address(segment: u16, offset: u16) -> usize {
@@ -14,15 +18,29 @@ impl BUS {
     }
 
     pub fn get_memory(&mut self, _cpu: &mut crate::cpu::CPU, address: usize) -> *mut u8 {
-        std::ptr::null_mut()
+        if address < self.ram.capacity() {
+            &mut self.ram[address]
+        } else {
+            &mut self.rom[0]
+        }
     }
 
     pub fn read_from_memory(&mut self, cpu: &mut crate::cpu::CPU, src: *const u8, data_width: u8) -> u32 {
-        0
+        match data_width {
+            8 => unsafe { *src as u32 },
+            16 => unsafe { *(src as *const u16) as u32 },
+            32 => unsafe { *(src as *const u32) },
+            _ => 0
+        }
     }
 
     pub fn write_to_memory(&mut self, cpu: &mut crate::cpu::CPU, dst: *mut u8, data_width: u8, value: u32) {
-
+        match data_width {
+            8 => unsafe { *dst = value as u8; },
+            16 => unsafe { *(dst as *mut u16) = value as u16; },
+            32 => unsafe { *(dst as *mut u32) = value; },
+            _ => { }
+        }
     }
 
     pub fn read_from_port(&mut self, cpu: &mut crate::cpu::CPU, address: u16) -> u8 {
